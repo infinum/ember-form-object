7a070a56ac94990c00bb1e090dba9fa20adf2618
Ivan Lucin <lucin.ivan@gmail.com>
2016-04-13 15:56:45 +0200

    Fix tests owner stub

--- tests/stubs/owner.js
+++ tests/stubs/owner.js
@@ -1,13 +1,27 @@
 import Ember from 'ember';
 
+const hasGetOwner = !!Ember.getOwner;
+
+function setOwnerPolyfill(object, owner) {
+  if (hasGetOwner) {
+    return Ember.setOwner(object, owner);
+  }
+
+  object.container = {
+    lookup: owner.lookup,
+    resolveRegistration: owner.resolveRegistration,
+    registry: owner,
+    _registry: owner
+  };
+
+  return object;
+}
+
 function setOwnerWithRegistry(object, registryResolveMap, lookupResolveMap) {
-  Ember.setOwner(object, {
-    lookup(moduleId) {
-      return lookupResolveMap[moduleId];
-    },
-    resolveRegistration(moduleId) {
-      return registryResolveMap[moduleId];
-    }
+  setOwnerPolyfill(object, {
+    lookup: (moduleId) => lookupResolveMap[moduleId],
+    resolveRegistration: (moduleId) => registryResolveMap[moduleId],
+    resolve: (moduleId) => registryResolveMap[moduleId]
   });
 }
 
