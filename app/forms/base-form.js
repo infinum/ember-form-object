9db77a62a863794fab80d70108dddf041a798558
Ivan Lucin <lucin.ivan@gmail.com>
2016-04-30 19:34:02 +0200

    Remove base & model form from app space

--- app/forms/base-form.js
+++
@@ -1,2 +0,0 @@
-import BaseForm from 'ember-form-object/forms/base-form';
-export default BaseForm;
--- app/forms/model-form.js
+++
@@ -1,2 +0,0 @@
-import ModelForm from 'ember-form-object/forms/model-form';
-export default ModelForm;
