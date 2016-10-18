fd00b6f78cb6dfb4f4155c3686a2f8c021455e3c
Ivan Lucin <lucin.ivan@gmail.com>
2016-10-18 13:59:42 +0200

    Add form loss preventer tests in form route mixin

--- tests/unit/mixins/form-route-test.js
+++ tests/unit/mixins/form-route-test.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 import BaseFormObject from 'ember-form-object/forms/base-form';
 import ModelFormObject from 'ember-form-object/forms/model-form';
 import FormRouteMixin from 'ember-form-object/mixins/form-route';
+import FormLossPreventerService from 'ember-form-object/services/form-loss-preventer';
 import { module, test } from 'qunit';
 import { setOwnerWithRegistry } from '../../stubs/owner';
 import { TestModel } from '../../stubs/model';
+
+const {run} = Ember;
 
 module('Unit | Mixin | form route', {
   beforeEach() {
@@ -14,12 +17,16 @@ module('Unit | Mixin | form route', {
 
     this.route = FormRoute.create();
 
-    this.baseFormObjectClass = BaseFormObject.extend({});
+    this.baseFormObjectClass = BaseFormObject.extend({properties: { test: {} }});
     this.modelFormObjectClass = ModelFormObject.extend({});
 
     setOwnerWithRegistry(this.route, {
       'form:base': this.baseFormObjectClass,
       'form:model': this.modelFormObjectClass
+    }, {
+      'service:form-loss-preventer': FormLossPreventerService.extend({
+        setupBeforeUnloadListener() {}
+      }).create()
     });
   }
 });
@@ -37,6 +44,19 @@ test('it should create form from form name', function(assert) {
   const form = this.route.createForm();
   assert.ok(form instanceof this.baseFormObjectClass);
   assert.equal(this.route.get('form'), form);
+});
+
+test('it should detect registered dirty form to prevent transition', function(assert) {
+  const form = this.route.createForm();
+  form.set('test', 'something');
+  assert.equal(this.route.shouldPreventTransition(), true);
+});
+
+test('it should not detect registered dirty form to prevent transition after form is destroyed', function(assert) {
+  const form = this.route.createForm();
+  form.set('test', 'something');
+  run(() => this.route.destroyForm());
+  assert.equal(this.route.shouldPreventTransition(), false);
 });
 
 test('it should create form with model in afterModel hook', function(assert) {
