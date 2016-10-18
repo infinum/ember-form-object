26d6bc7d6dd99a9f63e8fc84267aa0770c1dbb9d
Ivan Lucin <lucin.ivan@gmail.com>
2016-10-18 10:57:07 +0200

    Add form-loss-preventer service

---
+++ addon/services/form-loss-preventer.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import {some} from 'ember-form-object/utils/core';
+
+const {Service, computed, $, A: emberArray} = Ember;
+
+export default Service.extend({
+  registeredFormObjects: computed(() => emberArray()),
+
+  init() {
+    this._super(...arguments);
+    this.setupBeforeUnloadListener();
+  },
+
+  setupBeforeUnloadListener() {
+    $(window).on('beforeunload', (ev) => this.onWindowBeforeUnload(ev));
+  },
+
+  registerFormObject(formObject) {
+    this.set('registeredFormObjects', this.get('registeredFormObjects').concat(formObject));
+  },
+
+  unregisterFormObject(formObject) {
+    this.set('registeredFormObjects', this.get('registeredFormObjects').without(formObject));
+  },
+
+  hasSomeDirtyForms() {
+    return some(this.get('registeredFormObjects').map((formObject) => formObject.get('isDirty')));
+  },
+
+  onWindowBeforeUnload() {
+    if (this.hasSomeDirtyForms()) {
+      return 'You have attempted to leave this page. There are some changes that need to be saved.';
+    }
+  }
+});
